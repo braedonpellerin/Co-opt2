7d4c116d8eb6d2ac6d5910b1a75258783c15336a
braedonpellerin <94927239+braedonpellerin@users.noreply.github.com>
2023-10-30 01:40:11 -0500

    File upload

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Custom_Views_Coopt2"
+include(":app")
+ 
